5bd9715e53b1bd534e430609a0c436b40a6fbb13
Marwan Abdellah <abdellah.marwan@gmail.com>
2022-08-01 15:24:12 +0200

    Adding a simple UI script to test the integration of PySide6.

---
+++ examples/test_ui.py
@@ -0,0 +1,43 @@
+import random
+import sys
+
+from PySide6.QtCore import Qt, Slot
+from PySide6.QtWidgets import (QApplication, QLabel, QPushButton,
+                               QVBoxLayout, QWidget)
+from __feature__ import snake_case, true_property
+
+
+class MyWidget(QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+
+        self.hello = [
+            "Hallo",
+            "Hola",
+            "Hei maailma",
+            "Hola Mundo"
+        ]
+
+        self.button = QPushButton("Click me!")
+        self.message = QLabel("Hello World")
+        self.message.alignment = Qt.AlignCenter
+
+        self.layout = QVBoxLayout(self)
+        self.layout.add_widget(self.message)
+        self.layout.add_widget(self.button)
+
+        # Connecting the signal
+        self.button.clicked.connect(self.magic)
+
+    @Slot()
+    def magic(self):
+        self.message.text = random.choice(self.hello)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    widget = MyWidget()
+    widget.show()
+
+    sys.exit(app.exec_())
