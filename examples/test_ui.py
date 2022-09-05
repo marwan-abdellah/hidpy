e002e941b34c05e8ae6243e4a1b594b1c998fb6f
Marwan Abdellah <abdellah.marwan@gmail.com>
2022-09-05 21:56:02 +0200

    Adding the layouts with the Threads.

--- examples/test_ui.py
+++ examples/test_ui.py
@@ -1,63 +1,151 @@
-import random
-import sys
+import sys, time
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+from PySide6.QtWidgets import * 
 
-from PySide6.QtCore import Qt, Slot
-from PySide6.QtWidgets import (QApplication, QLabel, QPushButton,
-                               QVBoxLayout, QWidget, QMainWindow)
-from __feature__ import snake_case, true_property
+class MySignal(QObject):
+    sig = Signal(str)
 
+class MyLongThread(QThread):
 
-class MyWindow(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self)
+    def __init__(self, parent = None):
+        QThread.__init__(self, parent)
+        self.exiting = False
+        self.signal = MySignal()
 
-        self.hello = [
-            "Hallo",
-            "Hola",
-            "Hei maailma",
-            "Hola Mundo"
-        ]
+    def run(self):
+        end = time.time()+10
+        while self.exiting==False:
+                sys.stdout.write('*')
+                sys.stdout.flush()
+                time.sleep(1)
+                now = time.time()
+                if now>=end:
+                        self.exiting=True
+        self.signal.sig.emit('OK')
 
-        self.button = QPushButton("Click me!")
-        self.message = QLabel("Hello World")
-        self.message.alignment = Qt.AlignCenter
+class MyThread(QThread):
 
-        self.layout = QVBoxLayout(self)
-        self.layout.add_widget(self.message)
-        self.layout.add_widget(self.button)
+    def __init__(self, parent = None):
+        QThread.__init__(self, parent)
+        self.exiting = False
 
-        # Connecting the signal
-        self.button.clicked.connect(self.magic)
-
-    @Slot()
-    def magic(self):
-        self.message.text = random.choice(self.hello)
-
+    def run(self):
+        while self.exiting==False:
+                sys.stdout.write('.')
+                sys.stdout.flush()
+                time.sleep(1)
 
 class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
 
-        self.setWindowTitle("My App")
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self,parent)
+        self.centralwidget = QWidget(self)
 
-        button = QPushButton("Press Me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked)
+        # Central layout 
+        self.vbox = QVBoxLayout()
 
-        # Set the central widget of the Window.
-        self.setCentralWidget(button)
-
-    def the_button_was_clicked(self):
-        print("Clicked!")
+        # Input parameters area 
+        self.parameters_area = QVBoxLayout()
+        self.l1 = QLabel('Parameters')
+        self.parameters_area.addWidget(self.l1)
 
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
 
-    window = MainWindow()
-    window.show()
 
-    #widget = MyWidget()
-    #widget.show()
+        self.vbox.addLayout(self.parameters_area)
+        
+        self.output_area = QVBoxLayout()
+        self.batchbutton = QPushButton('Start batch',self)
+        self.longbutton = QPushButton('Start long (10 seconds) operation',self)
+        self.label1 = QLabel('Continuos batch')
+        self.label2 = QLabel('Long batch')
 
-    sys.exit(app.exec_())
+
+        self.lineedit = QLineEdit(parent=self)
+
+        self.image = QImage('/projects/hidpy/output/trajectories.png')
+
+        self.output_area.addWidget(self.batchbutton)
+        self.output_area.addWidget(self.longbutton)
+        self.output_area.addWidget(self.label1)
+        self.output_area.addWidget(self.label2)
+        self.output_area.addWidget(self.lineedit)
+        
+
+        self.image_label = QLabel(" ")
+        self.image_label.setPixmap(QPixmap.fromImage(self.image))
+
+        self.output_area.addWidget(self.image_label)
+        
+
+        self.vbox.addLayout(self.output_area)
+
+
+
+        self.setCentralWidget(self.centralwidget)
+        self.centralwidget.setLayout(self.vbox)
+        self.thread = MyThread()
+        
+        
+        '''
+        self.longthread = MyLongThread()
+        self.batchbutton.clicked.connect(self.handletoggle)
+        self.longbutton.clicked.connect(self.longoperation)
+        self.thread.started.connect(self.started)
+        self.thread.finished.connect(self.finished)
+        self.longthread.signal.sig.connect(self.longoperationcomplete)
+        '''
+
+        self.lineedit.textChanged.connect(self.printValue)
+
+        
+    def printValue(self):
+        print(self.lineedit.text())
+
+    def started(self):
+        self.label1.setText('Continuous batch started')
+
+    def finished(self):
+        self.label1.setText('Continuous batch stopped')
+
+    def terminated(self):
+        self.label1.setText('Continuous batch terminated')
+
+    def handletoggle(self):
+        if self.thread.isRunning():
+                self.thread.exiting=True
+                self.batchbutton.setEnabled(False)
+                while self.thread.isRunning():
+                        time.sleep(0.01)
+                        continue
+                self.batchbutton.setText('Start batch')
+                self.batchbutton.setEnabled(True)
+        else:
+                self.thread.exiting=False
+                self.thread.start()
+                self.batchbutton.setEnabled(False)
+                while not self.thread.isRunning():
+                        time.sleep(0.01)
+                        continue
+                self.batchbutton.setText('Stop batch')
+                self.batchbutton.setEnabled(True)
+
+    def longoperation(self):
+        if not self.longthread.isRunning():
+                self.longthread.exiting=False
+                self.longthread.start()
+                self.label2.setText('Long operation started')
+                self.longbutton.setEnabled(False)
+
+    def longoperationcomplete(self,data):
+        self.label2.setText('Long operation completed with: '+data)
+        self.longbutton.setEnabled(True)
+
+if __name__=='__main__':
+        app = QApplication(sys.argv)
+        app.setApplicationDisplayName('Hidpy')
+
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec_())
