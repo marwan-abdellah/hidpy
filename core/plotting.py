9625b47f019f24544fdf25c6400bfeef1f5cb3c6
Marwan Abdellah <abdellah.marwan@gmail.com>
2022-10-21 15:10:01 +0200

    Improving the plotting.

--- core/plotting.py
+++ core/plotting.py
@@ -24,6 +24,25 @@ def verify_plotting_packages():
 
 
 ####################################################################################################
+# @sample_range
+####################################################################################################
+def sample_range(start,
+                 end,
+                 steps):
+
+    # Delta
+    delta = 1. * (end - start) / (steps - 1)
+
+    # Data
+    data = list()
+    for i in range(steps):
+        value = start + i * delta
+        data.append(value)
+
+    return data
+
+
+####################################################################################################
 # @plot_trajectories_on_frame
 ####################################################################################################
 def plot_trajectories_on_frame(frame, trajectories, output_path):
@@ -104,15 +123,201 @@ def plot_trajectories(size, trajectories, output_path):
     cv2.imwrite('%s.png' % output_path, np_image)
 
 
-def plot_frame(frame, output_directory, frame_prefix):
+####################################################################################################
+# @plot_frame
+####################################################################################################
+def plot_frame(frame, output_directory, frame_prefix, font_size=10):
 
     verify_plotting_packages()
     
+    seaborn.set_style("whitegrid")
+    pyplot.rcParams['axes.grid'] = 'True'
+    pyplot.rcParams['grid.linewidth'] = 0.5
+    pyplot.rcParams['grid.color'] = 'black'
+    pyplot.rcParams['grid.alpha'] = 0.25
+    pyplot.rcParams['font.family'] = 'NimbusSanL'
+    pyplot.rcParams['font.monospace'] = 'Regular'
+    pyplot.rcParams['font.style'] = 'normal'
+    pyplot.rcParams['axes.labelweight'] = 'light'
+    pyplot.rcParams['axes.linewidth'] = 0.5
+    pyplot.rcParams['axes.labelsize'] = font_size
+    pyplot.rcParams['xtick.labelsize'] = font_size
+    pyplot.rcParams['ytick.labelsize'] = font_size
+    pyplot.rcParams['legend.fontsize'] = font_size
+    pyplot.rcParams['figure.titlesize'] = font_size
+    pyplot.rcParams['axes.titlesize'] = font_size
+    pyplot.rcParams['xtick.major.pad'] = '1'
+    pyplot.rcParams['ytick.major.pad'] = '1'
+    pyplot.rcParams['axes.edgecolor'] = '0'
+    pyplot.rcParams['axes.autolimit_mode'] = 'round_numbers'
 
 
+    # Plot 
     fig, ax = pyplot.subplots()
-    ax.imshow(frame)
-    pyplot.savefig('/projects/hidpy/output2/xx.png', dpi=300, bbox_inches='tight')
+    
+    # Create the ticks of the images 
+    xticks = sample_range(0, frame.shape[0], 5)
+    yticks = sample_range(0, frame.shape[1], 5)
 
-    pass 
+    # Show the image 
+    im = pyplot.imshow(frame)
+    ax.set_xlim(xticks[0], xticks[-1])
+    ax.set_ylim(yticks[0], yticks[-1])
+    ax.set_xticks(xticks)
+    ax.set_yticks(yticks)
+    
+    
+    # Color-basr axis 
+    cax = ax.inset_axes([0.00, -0.15, 1.0, 0.05])
 
+    # Create the ticks based on the range 
+    cbticks = sample_range((frame.min()), (frame.max()), 4)
+    cbticks = list(map(int, cbticks))
+
+    # Convert the ticks to a numpy array 
+    cbticks = numpy.array(cbticks)
+    
+    # Color-bar 
+    cb = pyplot.colorbar(im, ax=ax, cax=cax, orientation="horizontal", ticks=cbticks)
+    cb.ax.tick_params(labelsize=int(font_size * 0.75), width=0.5) 
+    cb.ax.set_xlim((cbticks[0], cbticks[-1]))
+    cb.update_ticks()
+
+    # Save the figure 
+    pyplot.savefig('%s/%s.png' % (output_directory, frame_prefix), dpi=300, bbox_inches='tight', pad_inches=0)
+
+
+
+def plot_matrix(matrix, output_directory, frame_prefix, font_size=10):
+
+    from matplotlib import colors
+
+
+    verify_plotting_packages()
+    
+    seaborn.set_style("whitegrid")
+    pyplot.rcParams['axes.grid'] = 'True'
+    pyplot.rcParams['grid.linewidth'] = 0.5
+    pyplot.rcParams['grid.color'] = 'black'
+    pyplot.rcParams['grid.alpha'] = 0.25
+    pyplot.rcParams['font.family'] = 'NimbusSanL'
+    pyplot.rcParams['font.monospace'] = 'Regular'
+    pyplot.rcParams['font.style'] = 'normal'
+    pyplot.rcParams['axes.labelweight'] = 'light'
+    pyplot.rcParams['axes.linewidth'] = 0.5
+    pyplot.rcParams['axes.labelsize'] = font_size
+    pyplot.rcParams['xtick.labelsize'] = font_size
+    pyplot.rcParams['ytick.labelsize'] = font_size
+    pyplot.rcParams['legend.fontsize'] = font_size
+    pyplot.rcParams['figure.titlesize'] = font_size
+    pyplot.rcParams['axes.titlesize'] = font_size
+    pyplot.rcParams['xtick.major.pad'] = '1'
+    pyplot.rcParams['ytick.major.pad'] = '1'
+    pyplot.rcParams['axes.edgecolor'] = '0'
+    pyplot.rcParams['axes.autolimit_mode'] = 'round_numbers'
+
+    # Plot 
+    fig, ax = pyplot.subplots()
+    
+    # Create the ticks of the images 
+    xticks = sample_range(0, matrix.shape[0], 5)
+    yticks = sample_range(0, matrix.shape[1], 5)
+
+    cmap = colors.ListedColormap(['w','g','b','purple','r','greenyellow'])
+
+    # Show the image 
+    im = pyplot.imshow(matrix, cmap=cmap)
+    ax.set_xlim(xticks[0], xticks[-1])
+    ax.set_ylim(yticks[0], yticks[-1])
+    ax.set_xticks(xticks)
+    ax.set_yticks(yticks)
+    
+    
+    
+    # Color-basr axis 
+    cax = ax.inset_axes([0.00, -0.15, 1.0, 0.05])
+
+    # Create the ticks based on the range 
+    cbticks = sample_range((frame.min()), (frame.max()), 4)
+    cbticks = list(map(int, cbticks))
+
+    # Convert the ticks to a numpy array 
+    cbticks = numpy.array(cbticks)
+    
+    # Color-bar 
+    cb = pyplot.colorbar(im, ax=ax, cax=cax, orientation="horizontal", ticks=cbticks)
+    cb.ax.tick_params(labelsize=int(font_size * 0.75), width=0.5) 
+    cb.ax.set_xlim((cbticks[0], cbticks[-1]))
+    cb.update_ticks()
+
+    # Save the figure 
+    pyplot.savefig('%s/%s.png' % (output_directory, frame_prefix), dpi=300, bbox_inches='tight', pad_inches=0)
+
+
+
+
+####################################################################################################
+# @plot_frame
+####################################################################################################
+def plot_labels_map(labels_map, output_directory, frame_prefix, font_size=10, npop=1):
+
+
+    from matplotlib import colors, pyplot
+    import seaborn
+
+    import numpy
+    seaborn.set_style("whitegrid")
+    pyplot.rcParams['axes.grid'] = 'True'
+    pyplot.rcParams['grid.linewidth'] = 0.5
+    pyplot.rcParams['grid.color'] = 'black'
+    pyplot.rcParams['grid.alpha'] = 0.25
+    pyplot.rcParams['font.family'] = 'NimbusSanL'
+    pyplot.rcParams['font.monospace'] = 'Regular'
+    pyplot.rcParams['font.style'] = 'normal'
+    pyplot.rcParams['axes.labelweight'] = 'light'
+    pyplot.rcParams['axes.linewidth'] = 0.5
+    pyplot.rcParams['axes.labelsize'] = font_size
+    pyplot.rcParams['xtick.labelsize'] = font_size
+    pyplot.rcParams['ytick.labelsize'] = font_size
+    pyplot.rcParams['legend.fontsize'] = font_size
+    pyplot.rcParams['figure.titlesize'] = font_size
+    pyplot.rcParams['axes.titlesize'] = font_size
+    pyplot.rcParams['xtick.major.pad'] = '1'
+    pyplot.rcParams['ytick.major.pad'] = '1'
+    pyplot.rcParams['axes.edgecolor'] = '0'
+    pyplot.rcParams['axes.autolimit_mode'] = 'round_numbers'
+
+    bounds = [0.5, 1.5, 2.5, 3.5, 4.5]
+
+    pyplot.clf
+
+    # Plot 
+    fig, ax = pyplot.subplots()
+    
+    # Create the ticks of the images 
+    xticks = sample_range(0, labels_map.shape[0], 5)
+    yticks = sample_range(0, labels_map.shape[1], 5)
+
+    ax.set_xticks(xticks)
+    ax.set_yticks(yticks)
+    
+
+    listcolors=['w','g','b','purple','r','greenyellow']
+    cmap = colors.ListedColormap(listcolors[0:npop+1])
+
+    img1=ax.imshow(labels_map, interpolation='nearest',cmap=cmap,origin='lower')
+    # Show the image 
+    
+     # Color-basr axis 
+    cax = ax.inset_axes([0.00, -0.15, 1.0, 0.05])
+
+    cbar=fig.colorbar(img1, ax=ax,spacing='proportional',orientation='horizontal',boundaries=[-0.5] + bounds[0:npop+1] + [npop+0.5], cax=cax)
+    cbar.outline.set_color('black')
+    cbar.outline.set_linewidth(1)
+    labels_cbar = numpy.arange(0, npop+1, 1)
+    loc = labels_cbar
+    cbar.set_ticks(loc)
+
+
+    # Save the figure 
+    pyplot.savefig('%s/%s.png' % (output_directory, frame_prefix), dpi=300, bbox_inches='tight', pad_inches=0)
--- deconvolution/importDeconvolutionGMM.py
+++ deconvolution/importDeconvolutionGMM.py
@@ -41,6 +41,20 @@ pyplot.rcParams['ytick.major.pad'] = '0'
 pyplot.rcParams['axes.edgecolor'] = '1'
 """
 
+def sample_range(start,
+                 end,
+                 steps):
+
+    # Delta
+    delta = 1. * (end - start) / (steps - 1)
+
+    # Data
+    data = list()
+    for i in range(steps):
+        value = start + i * delta
+        data.append(value)
+
+    return data
 
 ####################################################################################################
 # @verify_plotting_packages
@@ -114,16 +128,30 @@ def applyGMMconstrained_dir(listdir,parameters2decon,DistributionType,numDist):
     
     return BayesMat
 
-def generateplots_TestGMM(pathBayesCells_Plots,BayesMat,parameters2decon,nbins,showplots):
+def generateplots_TestGMM(pathBayesCells_Plots,BayesMat, parameters,nbins,showplots):
 
-    verify_plotting_packages()
+    import numpy as np
+    import pickle
+    from tqdm import tqdm
+    import os
+    import pandas as pd
+    from matplotlib import pyplot as plt
+    from scipy.stats import iqr
+    from matplotlib import colors
+
+    import seaborn
+    from matplotlib import pyplot
+    import matplotlib.pyplot as pyplot
+    import matplotlib.font_manager as font_manager
+    from matplotlib.ticker import FuncFormatter
+
+    #verify_plotting_packages()
     
     font_size = 10
     seaborn.set_style("whitegrid")
-    pyplot.rcParams['axes.grid'] = 'True'
-    pyplot.rcParams['grid.linestyle'] = '--'
+    pyplot.rcParams['axes.grid'] = 'False'
     pyplot.rcParams['grid.linewidth'] = 1.0
-    pyplot.rcParams['grid.color'] = 'gray'
+    pyplot.rcParams['grid.color'] = 'black'
     pyplot.rcParams['grid.alpha'] = 0.25
     pyplot.rcParams['font.family'] = 'Helvetica LT Std'
     pyplot.rcParams['font.family'] = 'NimbusSanL'
@@ -138,70 +166,111 @@ def generateplots_TestGMM(pathBayesCells_Plots,BayesMat,parameters2decon,nbins,s
     pyplot.rcParams['figure.titlesize'] = font_size
     pyplot.rcParams['axes.titlesize'] = font_size
     pyplot.rcParams['xtick.major.pad'] = '10'
-    pyplot.rcParams['ytick.major.pad'] = '0'
+    pyplot.rcParams['ytick.major.pad'] = '1'
     pyplot.rcParams['axes.edgecolor'] = '1'
 
     for i in tqdm(range(len(BayesMat))):
+        
 
-        filename=BayesMat[i]['filename']
-        filename_without_ext = os.path.splitext(filename)[0]
-
-        fig, axs = plt.subplots(1, len(parameters2decon))
-        fig.clf
-        fig.suptitle('Filename: '+os.path.basename(filename_without_ext), fontsize=10)
+        colors = ['r', 'g', 'b']
+        # Get the file name 
+        file_name = BayesMat[i]['filename']
+        
+        # File name without extension 
+        filename_without_ext = os.path.splitext(file_name)[0]
 
         
-        for count3 in range(len(parameters2decon)):
-            parameter2analyse=parameters2decon[count3]
-            xdata=[]
-            x=[]
+        figure_width = 8
+        figure_height = 3
+        # Create the new plot 
+        fig, axs = plt.subplots(1, len(parameters), figsize=(figure_width, figure_height))
+        fig.clf
+        fig.suptitle('Result') #'Filename: '+os.path.basename(filename_without_ext), fontsize=10)
+ 
+        # For every parameter that should be deconvolved 
+        for i_paramter in range(len(parameters)):
 
-            xdata=BayesMat[i][parameter2analyse].reshape(-1, 1)
-            xdata[np.where(np.isnan(xdata))]=0
-            xdata=xdata[np.where(xdata>1e-10)]
-
-            n,bins,patches=axs[count3].hist(xdata, edgecolor='c', color='c', density=True, bins=nbins, alpha=0.3);
-            x=arange(min(bins),max(bins),bins[1]-bins[0])
-
-            weights= BayesMat[i]['Deconvolution'][parameter2analyse]['weights']
-            mu=BayesMat[i]['Deconvolution'][parameter2analyse]['mu']
-            sigma=BayesMat[i]['Deconvolution'][parameter2analyse]['sigma']
-            DistributionType=BayesMat[i]['Deconvolution'][parameter2analyse]['DistributionType']
-            number_populations=BayesMat[i]['Deconvolution'][parameter2analyse]['number_populations']
-            model0=BayesMat[i]['Deconvolution'][parameter2analyse]['model']
+            # Get the parameter that needs analysis 
+            parameter2analyse = parameters[i_paramter]
             
-            axs[count3].set_title('DistType: '+DistributionType + ', # Populations: '+str(number_populations),fontsize=6)
-            axs[count3].set_xlabel(parameter2analyse, fontsize=8)
+            # Lists 
+            xdata = list()
+            x = list()
+
+            # MAGIC
+            xdata = BayesMat[i][parameter2analyse].reshape(-1, 1)
+            xdata[np.where(np.isnan(xdata))] = 0
+            xdata = xdata[np.where(xdata > 1e-10)]
+
+            # Construct the histogram 
+            n, bins, patches = axs[i_paramter].hist(xdata, edgecolor=colors[i_paramter], color=colors[i_paramter], density=True, bins=nbins, alpha=0.3)
+            x = np.arange(min(bins), max(bins), bins[1] - bins[0])
+
             
-            axs[count3].xaxis.set_tick_params(labelsize=10)
-            axs[count3].yaxis.set_tick_params(labelsize=10)
+            weights = BayesMat[i]['Deconvolution'][parameter2analyse]['weights']
+            mu = BayesMat[i]['Deconvolution'][parameter2analyse]['mu']
+            sigma = BayesMat[i]['Deconvolution'][parameter2analyse]['sigma']
+            DistributionType = BayesMat[i]['Deconvolution'][parameter2analyse]['DistributionType']
+            number_populations = BayesMat[i]['Deconvolution'][parameter2analyse]['number_populations']
+            model0 = BayesMat[i]['Deconvolution'][parameter2analyse]['model']
+            
+            axs[i_paramter].set_title('') #'DistType: '+DistributionType + ', # Populations: '+str(number_populations),fontsize=6)
+            axs[i_paramter].set_xlabel(parameter2analyse, fontsize=10)
+            
+            axs[i_paramter].xaxis.set_tick_params(labelsize=font_size)
+            axs[i_paramter].yaxis.set_tick_params(labelsize=font_size)
+
+            axs[i_paramter].xaxis.set_visible(True)
+            axs[i_paramter].yaxis.set_visible(True)
+
+            # Adjust the spines
+            axs[i_paramter].spines["bottom"].set_color('black')
+            axs[i_paramter].spines['bottom'].set_linewidth(1)
+            axs[i_paramter].spines["left"].set_color('black')
+            axs[i_paramter].spines['left'].set_linewidth(1)
+
+            # Plot the ticks
+            axs[i_paramter].tick_params(axis='x', width=1, which='both', bottom=True)
+            axs[i_paramter].tick_params(axis='y', width=1, which='both', left=True)
+
+            xticks = sample_range(min(bins), max(bins), 3)
+            axs[i_paramter].set_xlim(xticks[0], xticks[-1])
+            axs[i_paramter].set_xticks(xticks)
+
+            import math
+            difference = axs[i_paramter].get_ylim()[1] - axs[i_paramter].get_ylim()[0]
+            yticks = sample_range(axs[i_paramter].get_ylim()[0], axs[i_paramter].get_ylim()[1] + (difference * 0.1), 4)
+            axs[i_paramter].set_ylim(yticks[0], yticks[-1])
+            axs[i_paramter].set_yticks(yticks)
+
+            #axs[i_paramter].set_ylim(yticks[0], yticks[-1])
+
             
             tempval=np.zeros(x.shape)
 
             if DistributionType == 'normal':
                 for d in range(int(number_populations)):
-                    axs[count3].plot(x, weights[d]*applyGMM_functions.normal(x, mu[d], sigma[d]))
+                    axs[i_paramter].plot(x, weights[d]*applyGMM_functions.normal(x, mu[d], sigma[d]))
                     tempval= tempval+weights[d]*applyGMM_functions.normal(x, mu[d], sigma[d])
-                axs[count3].plot(x, tempval,'k')
+                axs[i_paramter].plot(x, tempval,'k')
 
             else:
                 if number_populations==1:
                     param = model0.parameters
-                    axs[count3].plot(x, weights[0]*applyGMM_functions.lognormal(x,param[0],param[1]))
+                    axs[i_paramter].plot(x, weights[0]*applyGMM_functions.lognormal(x,param[0],param[1]))
                 else:
                     for d in range(int(number_populations)):           
                         param = model0.distributions[d].parameters
-                        axs[count3].plot(x, weights[d]*applyGMM_functions.lognormal((x), param[0],param[1]))
+                        axs[i_paramter].plot(x, weights[d]*applyGMM_functions.lognormal((x), param[0],param[1]))
                         tempval= tempval+weights[d]*applyGMM_functions.lognormal((x), param[0],param[1])
-                    axs[count3].plot(x, tempval,'k')
+                    axs[i_paramter].plot(x, tempval,'k')
 
             fig.tight_layout(pad=0.5)
         
-        fig.savefig(pathBayesCells_Plots+os.path.basename(filename_without_ext)+'.png')
+        fig.savefig(pathBayesCells_Plots+os.path.basename(filename_without_ext)+'.png', dpi=300, bbox_inches='tight', pad_inches=0)
         if not(showplots):
             close(fig)
-        
-    return
+    
 
 def generatetable_TestGMM(pathBayesCells,BayesMat,parameters2decon):
     
@@ -358,6 +427,27 @@ def generate_plots_stats_decon(BayesMatSel,param,pathBayesCells_Populations_Plot
     print('Verify Package!')
 
     verify_plotting_packages()
+    font_size = 10
+    seaborn.set_style("whitegrid")
+    pyplot.rcParams['axes.grid'] = 'False'
+    pyplot.rcParams['grid.linewidth'] = 1.0
+    pyplot.rcParams['grid.color'] = 'black'
+    pyplot.rcParams['grid.alpha'] = 0.25
+    pyplot.rcParams['font.family'] = 'Helvetica LT Std'
+    pyplot.rcParams['font.family'] = 'NimbusSanL'
+    pyplot.rcParams['font.monospace'] = 'Regular'
+    pyplot.rcParams['font.style'] = 'normal'
+    pyplot.rcParams['axes.labelweight'] = 'light'
+    pyplot.rcParams['axes.linewidth'] = 1.0
+    pyplot.rcParams['axes.labelsize'] = font_size
+    pyplot.rcParams['xtick.labelsize'] = font_size
+    pyplot.rcParams['ytick.labelsize'] = font_size
+    pyplot.rcParams['legend.fontsize'] = font_size
+    pyplot.rcParams['figure.titlesize'] = font_size
+    pyplot.rcParams['axes.titlesize'] = font_size
+    pyplot.rcParams['xtick.major.pad'] = '10'
+    pyplot.rcParams['ytick.major.pad'] = '1'
+    pyplot.rcParams['axes.edgecolor'] = '1'
     
     bounds = [0.5, 1.5, 2.5, 3.5, 4.5]
 
@@ -397,6 +487,28 @@ def generate_plots_stats_decon(BayesMatSel,param,pathBayesCells_Populations_Plot
     cmap = colors.ListedColormap(listcolors[0:numPop+1])
 
     img1=ax[0].imshow(labels_map, interpolation='nearest',cmap=cmap,origin='lower')
+
+
+    ax[0].xaxis.set_visible(True)
+    ax[0].yaxis.set_visible(True)
+
+    # Adjust the spines
+    ax[0].spines["bottom"].set_color('black')
+    ax[0].spines['bottom'].set_linewidth(1)
+    ax[0].spines["left"].set_color('black')
+    ax[0].spines['left'].set_linewidth(1)
+
+    # Plot the ticks
+    ax[0].tick_params(axis='x', width=1, which='both', bottom=True)
+    ax[0].tick_params(axis='y', width=1, which='both', left=True)
+
+
+
+
+
+
+
+
     cbar=fig.colorbar(img1,ax=ax[0],spacing='proportional',orientation='horizontal',boundaries=[-0.5] + bounds[0:numPop+1] + [numPop+0.5])
     labels_cbar = np.arange(0, numPop+1, 1)
     loc = labels_cbar
@@ -459,5 +571,3 @@ def generate_plots_stats_decon(BayesMatSel,param,pathBayesCells_Populations_Plot
     dfTitle.to_csv(filename_csv, index=False, header=False)
     df.to_csv(filename_csv, mode='a')
 
-
-    return
